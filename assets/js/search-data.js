49a3af91cbb3ec76a88be3200511cf47b2681984
syedshaheerabbasrizvi <114511098+syedshaheerabbasrizvi@users.noreply.github.com>
2026-03-24 13:11:57 +0000

    Deploying to gh-pages from @ syedshaheerabbasrizvi/syedshaheerabbasrizvi.github.io@a2951c30292276508ad0db18fad680a2774456cf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-collaborative-human-robot-gantry-system",
+          title: 'Collaborative Human-Robot Gantry System',
+          description: "Industry 5.0 human-robot collaboration for microwave assembly at Dawlance",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
